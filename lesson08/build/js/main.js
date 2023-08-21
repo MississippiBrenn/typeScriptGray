c97f9ca097e90711121613fa3e3e6f4566cd07a1
MissppiBrennan <31112372+MissppiBrennan@users.noreply.github.com>
2023-08-20 17:03:09 -0700

    lesson 7

---
+++ lesson08/build/js/main.js
@@ -0,0 +1,38 @@
+"use strict";
+(, echo = (arg) => arg, , isObj = (arg) => {
+    return typeof arg === 'object' && !Array.isArray(arg) && arg !== null;
+}, console, log) => ;
+(isObj(true));
+console.log(isObj('John'));
+console.log(isObj([1, 2, 3]));
+console.log(isObj({ name: 'John' }));
+console.log(isObj(null));
+const isTrue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { arg, is: false };
+    }
+    return { arg, is: !!arg };
+};
+const checkBoolValue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { value: arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { value: arg, is: false };
+    }
+    return { value: arg, is: !!arg };
+};
+console.log(isTrue(false));
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue(1));
+console.log(isTrue('Dave'));
+console.log(isTrue(NaN));
+console.log(isTrue(-0));
+const processUser = (user), T, T;
+{
+    return user;
+}
