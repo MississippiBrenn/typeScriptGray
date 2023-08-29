18c75bf240161a725b610bda310bb53a1afb544d
MissppiBrennan <31112372+MissppiBrennan@users.noreply.github.com>
2023-08-29 08:21:29 -0700

    lesson17

--- lesson17/src/App.tsx
+++ lesson17/src/App.tsx
@@ -1,30 +1,23 @@
-import Header from "../components/Header"
-import Footer from "../components/Footer"
-import Cart from "../components/Cart"
-import ProductList from "../components/ProductList"
-import { useState} from "react"
-
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Cart from "./components/Cart"
+import ProductList from "./components/ProductList"
+import { useState } from "react"
 
 function App() {
   const [viewCart, setViewCart] = useState<boolean>(false)
-  
-  const pageContext = viewCart ? <Cart /> : <ProductList />
 
-  const content =(
+  const pageContent = viewCart ? <Cart /> : <ProductList />
+
+  const content = (
     <>
-      <Header viewCart={viewCart} setViewCart={setViewCart}></Header>
+      <Header viewCart={viewCart} setViewCart={setViewCart} />
       {pageContent}
-     <Footer viewCart={viewCart}></Footer>
-
-
+      <Footer viewCart={viewCart} />
     </>
   )
-  return (
-    content
-  )
+
+  return content
 }
 
 export default App
-
-
-// npx json-server -w data/products.json -p 3500
